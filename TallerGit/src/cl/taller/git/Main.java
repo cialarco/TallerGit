429bfc05806c3e996c676a8e096865bc578bc3b6
cialarco <cialarco@alumnos.ubiobio.cl>
2014-04-04 10:37:21 -0400

    Commit en la Web
    Commit en la Web

--- TallerGit/src/cl/taller/git/Main.java
+++ TallerGit/src/cl/taller/git/Main.java
@@ -6,6 +6,7 @@ public class Main {
 		// TODO Auto-generated method stub
 		System.out.println("Primer Commit");
 		System.out.println("Segundo Commit");
+		System.out.println("Linea Agregada en la WEB");
 	}
 
 }
